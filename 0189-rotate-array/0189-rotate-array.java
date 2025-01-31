1a908c0663e392cb7ae4c6259ce2766be447425a
Madivalappa Bani <msbani19@gmail.com>
2025-01-31 14:17:28 -0500

    Time: 0 ms (100%), Space: 57.2 MB (76.04%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+       k = k % nums.length;
+       if (k != 0) {
+        int[] temp = new int[nums.length];
+        System.arraycopy(nums, nums.length - k, temp, 0, k);
+        System.arraycopy(nums, 0, temp, k, nums.length - k);
+        System.arraycopy(temp, 0, nums, 0, nums.length);
+       } 
+    }
+}
