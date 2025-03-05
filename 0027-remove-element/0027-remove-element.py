2861378f4fe43a117c5c7e9af03fd85a4477697c
Madivalappa Bani <msbani19@gmail.com>
2025-03-05 09:26:18 -0500

    Time: 0 ms (100%), Space: 18 MB (11.37%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -3,8 +3,8 @@ class Solution:
         k = 0
 
         for i in range(len(nums)):
-            if nums[i] !=val:
+            if nums[i] != val:
                 nums[k] = nums[i]
-                k +=1
+                k += 1
 
         return k
