9d5fdcea7bb4d367e05c39c56deccc146dfd347a
Madivalappa Bani <msbani19@gmail.com>
2025-03-06 06:51:02 -0500

    Time: 0 ms (100%), Space: 57.2 MB (76.37%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -6,6 +6,6 @@ class Solution {
         System.arraycopy(nums, nums.length - k, temp, 0, k);
         System.arraycopy(nums, 0, temp, k, nums.length - k);
         System.arraycopy(temp, 0, nums, 0, nums.length);
-       } 
+       }
     }
 }
