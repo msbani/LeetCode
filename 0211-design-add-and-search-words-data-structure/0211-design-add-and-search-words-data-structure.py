934aae90799a64f53d07d23e6a2e9e47621c61ea
Madivalappa Bani <msbani19@gmail.com>
2025-04-06 12:36:41 -0400

    Time: 1270 ms (60.59%), Space: 65.2 MB (78.06%) - LeetHub

---
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,41 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class WordDictionary:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.is_end = True
+
+    def search(self, word: str) -> bool:
+        def dfs(index, node):
+            if index == len(word):
+                return node.is_end
+
+            char = word[index]
+            if char == ".":
+                for child in node.children.values():
+                    if dfs(index + 1, child):
+                        return True
+                return False
+            else:
+                if char not in node.children:
+                    return False
+                return dfs(index + 1, node.children[char])
+
+        return dfs(0, self.root)
+        
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
