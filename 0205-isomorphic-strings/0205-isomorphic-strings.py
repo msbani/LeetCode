e34f9278a7d5ed2cef7934f53c252d7c31310d54
Madivalappa Bani <msbani19@gmail.com>
2025-05-10 15:06:21 -0400

    Time: 3 ms (95.48%), Space: 17.9 MB (55.74%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,16 +1,16 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        char_index_s = {}
-        char_index_t = {}
+        char_s = {}
+        char_t = {}
 
         for i in range(len(s)):
-            if s[i] not in char_index_s:
-                char_index_s[s[i]] = i
+            if s[i] not in char_s:
+                char_s[s[i]] = i
 
-            if t[i] not in char_index_t:
-                char_index_t[t[i]] = i
+            if t[i] not in char_t:
+                char_t[t[i]] = i
 
-            if char_index_s[s[i]] != char_index_t[t[i]]:
+            if char_s[s[i]] != char_t[t[i]]:
                 return False
 
         return True
