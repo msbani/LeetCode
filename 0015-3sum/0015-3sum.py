262757756c5b41ca10aea7ede676776918acb7ce
Madivalappa Bani <msbani19@gmail.com>
2025-05-21 14:58:36 -0400

    Time: 625 ms (35.92%), Space: 20.5 MB (85.04%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-
+            
             j = i + 1
             k = len(nums) - 1
 
@@ -22,6 +22,6 @@ class Solution:
                     j += 1
 
                     while nums[j] == nums[j-1] and j < k:
-                        j+= 1
-
+                        j += 1
+        
         return res
