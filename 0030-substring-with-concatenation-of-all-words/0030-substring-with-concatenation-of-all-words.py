e7a002866bd60c334919f4c7826825d887da817f
Madivalappa Bani <msbani19@gmail.com>
2025-06-04 15:40:51 -0400

    Time: 7270 ms (27.05%), Space: 18.7 MB (60.32%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,16 +1,18 @@
+from collections import defaultdict
+
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         if not s or not words:
             return []
-
+        
         word_count = defaultdict(int)
         for word in words:
             word_count[word] += 1
-
+        
         substr_len = len(words) * len(words[0])
         word_len = len(words[0])
         result = []
-
+        
         for i in range(len(s) - substr_len + 1):
             seen = defaultdict(int)
             for j in range(i, i + substr_len, word_len):
@@ -23,5 +25,5 @@ class Solution:
                     break
             else:
                 result.append(i)
-
-        return result 
+        
+        return result
