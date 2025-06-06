cc8556c9fbbf83d41bb6e90057d06adca9a1e72b
Madivalappa Bani <msbani19@gmail.com>
2025-06-05 22:03:23 -0400

    Time: 25 ms (78.74%), Space: 18.9 MB (20.99%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,29 +1,33 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        if not s or not words:
-            return []
-        
-        word_count = defaultdict(int)
-        for word in words:
-            word_count[word] += 1
-        
-        substr_len = len(words) * len(words[0])
-        word_len = len(words[0])
-        result = []
-        
-        for i in range(len(s) - substr_len + 1):
-            seen = defaultdict(int)
-            for j in range(i, i + substr_len, word_len):
-                word = s[j:j+word_len]
-                if word in word_count:
-                    seen[word] += 1
-                    if seen[word] > word_count[word]:
-                        break
-                else:
-                    break
-            else:
-                result.append(i)
-        
-        return result
+        length = len(words[0])
+        word_count = Counter(words)
+        indexes = []
+
+        for i in range(length):
+            start = i
+            window = defaultdict(int)
+            words_used = 0
+
+            for j in range(i, len(s) - length + 1, length):
+                word = s[j:j + length]
+
+                if word not in word_count:
+                    start = j + length
+                    window = defaultdict(int)
+                    words_used = 0
+                    continue
+
+                words_used += 1
+                window[word] += 1
+
+                while window[word] > word_count[word]:
+                    window[s[start:start + length]] -= 1
+                    start += length
+                    words_used -= 1
+
+                if words_used == len(words):
+                    indexes.append(start)
+        return indexes
