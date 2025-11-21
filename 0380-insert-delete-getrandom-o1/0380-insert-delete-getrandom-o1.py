6ffcc42fd9e153bd370e5fd2ce652b08ac4a85f3
Madivalappa Bani <msbani19@gmail.com>
2025-11-21 11:56:05 -0600

    Time: 116 ms (75.54%), Space: 57.9 MB (64.32%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -1,5 +1,4 @@
 class RandomizedSet:
-
     def __init__(self):
         self.lst = []
         self.idx_map = {}
@@ -18,15 +17,13 @@ class RandomizedSet:
     def remove(self, val: int) -> bool:
         if not self.search(val):
             return False
-
+        
         idx = self.idx_map[val]
         self.lst[idx] = self.lst[-1]
         self.idx_map[self.lst[-1]] = idx
         self.lst.pop()
         del self.idx_map[val]
         return True
-
-        
 
     def getRandom(self) -> int:
         return random.choice(self.lst)
