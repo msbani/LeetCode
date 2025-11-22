720f33fc15d69cd2357bdb4cd7cdc4e5c63867a6
Madivalappa Bani <msbani19@gmail.com>
2025-11-22 10:55:46 -0600

    Time: 11 ms (71.19%), Space: 19.3 MB (69.3%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,5 +1,6 @@
+
 class Solution:
-    def trap(self, height: List[int]) -> int:
+    def trap(self, height: List[int])->int:
         left = 0
         right = len(height) - 1
         left_max = height[left]
@@ -11,6 +12,7 @@ class Solution:
                 left += 1
                 left_max = max(left_max, height[left])
                 water += left_max - height[left]
+
             else:
                 right -= 1
                 right_max = max(right_max, height[right])
