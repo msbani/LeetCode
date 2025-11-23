de990b45aeeaf6037bdb7459048bf5ed12f5f4c8
Madivalappa Bani <msbani19@gmail.com>
2025-11-23 13:57:32 -0600

    Time: 9 ms (70.1%), Space: 17.9 MB (68.81%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,9 +1,11 @@
+
 class Solution:
-    def convert(self, s: str, numRows: int) -> str:
+    def convert(self, s:str, numRows:int)->str:
         if numRows == 1 or numRows >= len(s):
             return s
 
-        idx, d = 0, 1
+        idx = 0
+        d = 1
         rows = [[] for _ in range(numRows)]
 
         for char in s:
@@ -18,3 +20,4 @@ class Solution:
             rows[i] = ''.join(rows[i])
 
         return ''.join(rows)
+
