f37f52d14ffe139e37d077ed46a455c4ed4b3359
Madivalappa Bani <msbani19@gmail.com>
2025-11-25 06:37:53 -0600

    Time: 0 ms (100%), Space: 17.8 MB (58.52%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,5 +1,6 @@
+
 class Solution:
-    def wordPattern(self, pattern: str, s: str) -> bool:
+    def wordPattern(self, pattern: str, s: str)-> bool:
         s = s.split()
 
         return (len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern, s))))
