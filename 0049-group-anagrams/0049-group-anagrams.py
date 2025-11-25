f49f4272e0dba356244ab1bcbbcfa8e2daf205aa
Madivalappa Bani <msbani19@gmail.com>
2025-11-25 07:21:28 -0600

    Time: 20 ms (25.52%), Space: 22.6 MB (31.5%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,5 +1,6 @@
+
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    def groupAnagrams(self, strs: List[str])->List[List[str]]:
         ans = defaultdict(list)
 
         for s in strs:
