debfc46f04d2a46003855e53e75ab7e20a078328
Madivalappa Bani <msbani19@gmail.com>
2025-11-28 07:35:24 -0600

    Time: 580 ms (16.44%), Space: 0B (100%) - LeetHub

---
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT product_id, 'store1' AS store,store1 AS price FROM products
+WHERE store1 IS NOT NULL
+UNION
+SELECT product_id, 'store2',store2 FROM Products
+WHERE store2 IS NOT NULL
+UNION
+SELECT product_id, 'store3', store3 FROM products
+WHERE store3 IS NOT NULL
