12278588405fcc99869ecb78131f2ae53eadbddf
Madivalappa Bani <msbani19@gmail.com>
2025-12-01 05:54:46 -0600

    Time: 38 ms (80.54%), Space: 27 MB (32.78%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,7 @@
 
 class Solution:
     def maxProfit(self, prices: List[int])-> int:
+
         buy = prices[0]
         profit = 0
 
@@ -11,5 +12,3 @@ class Solution:
                 profit = prices[i] - buy
 
         return profit
-
-
