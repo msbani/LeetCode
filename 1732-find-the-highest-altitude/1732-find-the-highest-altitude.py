9c4d788eb9bf5a14d2a3f8154bd24d6953ee0b9d
Madivalappa Bani <msbani19@gmail.com>
2025-12-14 05:38:11 -0600

    Time: 0 ms (100%), Space: 17.7 MB (81.39%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        gain = [0] + gain
+        max_alt = 0
+        
+        for i in range(len(gain)):
+            alt = max_alt + gain[i]
+            max_alt = alt
+            gain[i] = alt
+        return max(gain)
