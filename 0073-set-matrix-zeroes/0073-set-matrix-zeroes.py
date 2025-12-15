82ae04a72b609ac7f0fe71337347ac1047294429
Madivalappa Bani <msbani19@gmail.com>
2025-12-15 09:30:10 -0600

    Time: 3 ms (71.25%), Space: 18.2 MB (99.57%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,45 +3,25 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-
-        rows = len(matrix)
-        cols = len(matrix[0])
-
-        first_row_has_zero = False
-        first_col_has_zero = False
-
-        for c in range(cols):
-            if matrix[0][c] == 0:
-                first_row_has_zero = True
-                break
+        rows, cols = len(matrix), len(matrix[0])
+        rowZero = False
 
         for r in range(rows):
-            if matrix[r][0] == 0:
-                first_col_has_zero = True
-                break
-
+            for c in range(cols):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = True
         for r in range(1, rows):
             for c in range(1, cols):
-                if matrix[r][c] == 0:
-                    matrix[r][0] = 0
-                    matrix[0][c] = 0
-                    
-        for r in range(1, rows):
-            if matrix[r][0] == 0:
-                for c in range(1, cols):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
                     matrix[r][c] = 0
 
-        for c in range(1, cols):
-            if matrix[0][c] == 0:
-                for r in range(1, rows):
-                    matrix[r][c] = 0
-
-        if first_row_has_zero:
-            for c in range(cols):
-                matrix[0][c] = 0
-
-        if first_col_has_zero:
+        if matrix[0][0] == 0:
             for r in range(rows):
                 matrix[r][0] = 0
-
-        return matrix
+        if rowZero:
+            for c in range(cols):
+                matrix[0][c] = 0
