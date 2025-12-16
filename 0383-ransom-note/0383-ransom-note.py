2463788cf4b25ba0d008751dbec76427e11a3bf8
Madivalappa Bani <msbani19@gmail.com>
2025-12-16 06:10:34 -0600

    Time: 23 ms (47.75%), Space: 17.8 MB (81.75%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,11 +1,11 @@
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str)->bool:
-        if len(ransomNote) > len(magazine):
-            return False
-
-        for c in set(ransomNote):
-            if magazine.count(c) < ransomNote.count(c):
+        d = defaultdict(int)
+        for c in magazine:
+            d[c] += 1
+        for c in ransomNote:
+            if c not in d or d[c] <= 0:
                 return False
-
+            d[c] -= 1
         return True
