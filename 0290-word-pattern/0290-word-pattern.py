2fdba2f9cef98ae8195bb59986ba936f2ba6d239
Madivalappa Bani <msbani19@gmail.com>
2025-12-17 06:45:40 -0600

    Time: 0 ms (100%), Space: 17.7 MB (86.71%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.py
+++ 0290-word-pattern/0290-word-pattern.py
@@ -1,6 +1,18 @@
-
 class Solution:
     def wordPattern(self, pattern: str, s: str)-> bool:
-        s = s.split()
+        words = s.split()
 
-        return (len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern, s))))
+        if len(words) != len(pattern):
+            return False
+
+        charToWord = {}
+        wordToChar = {}
+        for c, w in zip(pattern, words):
+            if c in charToWord and charToWord[c] != w:
+                return False
+            if w in wordToChar and wordToChar[w] != c:
+                return False
+
+            charToWord[c] = w
+            wordToChar[w] = c
+        return True
