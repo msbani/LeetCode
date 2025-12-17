92f048eb19df98f606cd38186a19cde3739ec7f4
Madivalappa Bani <msbani19@gmail.com>
2025-12-17 06:53:49 -0600

    Time: 903 ms (5.81%), Space: 18 MB (47.9%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,4 +1,10 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str)-> bool:
-        return sorted(s) == sorted(t)
+        tl = list(t)
+        if len(s) != len(tl):
+            return False
+        for c in s:
+            if c in tl:
+                tl.remove(c)
+        return len(tl) == 0
