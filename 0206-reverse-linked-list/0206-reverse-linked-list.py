35910163179d66e7b8362f2503b90bb81ed9cae6
Madivalappa Bani <msbani19@gmail.com>
2025-12-28 06:19:25 -0600

    Time: 0 ms (100%), Space: 18.3 MB (87.68%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -3,13 +3,25 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         prev = None
+#         curr = head
+#         while curr:
+#             next_node = curr.next
+#             curr.next = prev
+#             prev = curr
+#             curr = next_node
+#         return prev
+
+# Recursive Solution
 class Solution:
+    def helper(self, prev, curr):
+        if curr is None:
+            return prev
+        next_node = curr.next
+        curr.next = prev
+        new_head = self.helper(curr, next_node)
+        return new_head
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = head
-        while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
-        return prev
+        return self.helper(None, head)
