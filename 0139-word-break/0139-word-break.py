97ede5eb7bd8da967494898c29886890a79a9e3e
Madivalappa Bani <msbani19@gmail.com>
2026-02-05 07:38:31 -0600

    Time: 0 ms (100%), Space: 19.5 MB (28.6%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,16 +1,5 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        # dp = [True] + [False] * len(s)
-
-        # for i in range(1, len(s) + 1):
-        #     for w in wordDict:
-        #         start = i - len(w)
-        #         if start >= 0 and dp[start] and s[start:i] == w:
-        #             dp[i] = True
-        #             break
-
-        # return dp[-1]
-
         dp = [True] + [False] * len(s)
 
         for i in range(1, len(s) + 1):
@@ -19,4 +8,5 @@ class Solution:
                 if start >= 0 and dp[start] and s[start:i] == w:
                     dp[i] = True
                     break
+
         return dp[-1]
