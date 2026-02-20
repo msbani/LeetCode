298c64e03902d250b21b10b0c1beb9aab4ad2200
Madivalappa Bani <msbani19@gmail.com>
2026-02-20 08:51:55 -0600

    Time: 15 ms (61.11%), Space: 20.9 MB (21.57%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findPoisonedDuration(self, timeSeries: List[int], duration: int) -> int:
+        if not timeSeries:
+            return 0
+
+        total = 0
+        for i in range(len(timeSeries) - 1):
+            diff = timeSeries[i+1] - timeSeries[i]
+            total += min(diff, duration)
+
+        total += duration
+
+        return total
