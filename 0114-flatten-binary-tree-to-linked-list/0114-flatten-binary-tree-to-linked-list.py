9c825ab0ab04555eef235221b31e57f6fc2058da
Madivalappa Bani <msbani19@gmail.com>
2026-03-03 10:58:00 -0600

    Time: 4 ms (3.46%), Space: 19.5 MB (67.53%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.py
@@ -9,10 +9,16 @@ class Solution:
         """
         Do not return anything, modify root in-place instead.
         """
-        curr = root
-        while curr:
-            if curr.left:
-                runner = curr.left
-                while runner.right: runner = runner.right
-                runner.right, curr.right, curr.left = curr.right, curr.left, None
-            curr = curr.right
+        def dfs(root):
+            if not root:
+                return None
+            leftTail = dfs(root.left)
+            rightTail = dfs(root.right)
+
+            if root.left:
+                leftTail.right = root.right
+                root.right = root.left
+                root.left = None
+            last = rightTail or leftTail or root
+            return last
+        dfs(root)
